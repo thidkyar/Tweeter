ba09496efddc251b7c956afe85522f3ce2cd44a4
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-05-23 18:29:36 -0400

    created function to post tweets and sort by newest - oldest

--- tweeter/public/scripts/app.js
+++ tweeter/public/scripts/app.js
@@ -5,54 +5,6 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: {
-        small: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        regular: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        large: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      handle: "@SirIsaac"
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: {
-        small: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        regular: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        large: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1461113959088
-  },
-  {
-    user: {
-      name: "Johann von Goethe",
-      avatars: {
-        small: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        regular: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        large: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      handle: "@johann49"
-    },
-    content: {
-      text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    created_at: 1461113796368
-  }
-];
 
 function createTweetElement(tweet) {
   const $tweet = $("<li>").addClass("tweet_box");
@@ -85,31 +37,77 @@ function createTweetElement(tweet) {
     .addClass("tweet-age")
     .text(tweet.created_at)
     .appendTo($footer);
+  const $fontDiv = $("<div>")
+    .addClass("icons")
+    .appendTo($footer)
+
+  // $("<i>")
+  //   .addClass("fas fa-flag")
+  //   .appendTo($fontDiv);
+  // $("<i>")
+  //   .addClass("fas fa-retweet")
+  //   .appendTo($fontDiv)
+  // $("<i>")
+  //   .addClass("fas fa-heart")
+  //   .appendTo($fontDiv)
+
 
   $(".tweets_list").append($tweet);
+
   return $tweet;
 }
 
 function renderTweets(tweets) {
   tweets.forEach(function(tweet) {
-    createTweetElement(tweet).appendTo("tweets_list")
+    createTweetElement(tweet).appendTo(".tweets_list");
     // $("tweets_list").append(createTweetElement(tweet));
   });
 }
 
 $(document).ready(function() {
-  renderTweets(data);
-  $('form').submit(function(event){
-  event.preventDefault();
-  var str = $("form").serialize();
+  // renderTweets(data);
+  $(".compose").on('click', function(event) {
+    $(".new-tweet").toggle("slow")
+  })
 
-  $.ajax({
-    url: '/tweets',
-    method: 'POST',
-    data: str,
-    success: function () {
-      console.log('Success: ');
+  loadTweets();
+  function loadTweets() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function(data) {
+        // console.log("Success: ");
+        renderTweets(data);
+      },
+      error: function(xhr) {
+        console.log("error");
+      }
+    });
   }
-});
-});
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let str = $("form").serialize();
+    if ($("textarea").val().length > 140) {
+      $(".counter").text("Message too long!");
+    } else if (!$("textarea").val().length) {
+      $(".counter").text("Please enter something!");
+    } else {
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: str,
+        success: function() {
+          $.ajax({
+            url: "/tweets",
+            method: "GET",
+            success: function(data) {
+              createTweetElement(data[0]).prependTo(".tweets_list");
+              $(".textarea").val('');
+              $(".counter").val('');
+            }
+          });
+        }
+      });
+    }
+  });
 });
