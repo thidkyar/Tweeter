cfcc40fb10183cb9b09a9f20030edb909d36d630
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-05-23 18:30:11 -0400

    removed few comments -

--- tweeter/public/scripts/composer-char-counter.js
+++ tweeter/public/scripts/composer-char-counter.js
@@ -6,10 +6,8 @@ $(document).ready(function() {
     const $counter = $(this).siblings('.counter')
     let length = maxLength - valLength;
     if (length < 0) {
-      // $('.counter').css('color', 'red');
       $counter.addClass('red-text');
     } else if (length >= 0) {
-      // $('.counter').css('color', 'black');
       $counter.removeClass('red-text');
     }
     $counter.text(length)
