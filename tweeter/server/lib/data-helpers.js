ec3feb1925470e7cebe7a2b249494fc7521bc6e7
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-05-24 12:10:27 -0400

    Implement mongoDB to store data

--- tweeter/server/lib/data-helpers.js
+++ tweeter/server/lib/data-helpers.js
@@ -6,22 +6,34 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
+    // saveTweet: function(newTweet, callback) {
+    //   simulateDelay(() => {
+    //     db.tweets.push(newTweet);
+    //     callback(null, true);
+    //   });
+    // },
+
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+      // simulateDelay() => {
+        db.collection('tweeter').insertOne(newTweet);
         callback(null, true);
-      });
-    },
+      },
 
     // Get all tweets in `db`, sorted by newest first
+    // getTweets: function(callback) {
+    //   simulateDelay(() => {
+    //     const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+    //     callback(null, db.tweets.sort(sortNewestFirst));
+    //   });
+    // }
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweeter").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets);
       });
     }
-
   };
-}
+};
