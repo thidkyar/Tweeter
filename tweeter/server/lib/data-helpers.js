0799f7cb7f9d388d6edfbfd9ce98a1c777079fdf
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-05-25 15:24:51 -0400

    add like tweet

--- tweeter/server/lib/data-helpers.js
+++ tweeter/server/lib/data-helpers.js
@@ -2,26 +2,32 @@
 
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
+const { ObjectId } = require("mongodb");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
-    // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      // simulateDelay() => {
+      db.collection("tweets").insertOne(newTweet);
+      callback(null, true);
     },
 
-    // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
-    }
+    likeTweet: function(id, callback) {
+      db.collection("tweets").updateOne({ _id: ObjectId(id) }, { $inc: { likes: 1 } });
+      callback(null, true);
+    },
 
+    getTweets: function(callback) {
+      db
+        .collection("tweets")
+        .find()
+        .toArray((err, tweets) => {
+          if (err) {
+            return callback(err);
+          }
+          callback(null, tweets);
+        });
+    }
   };
-}
+};
--- tweeter/server/routes/tweets.js
+++ tweeter/server/routes/tweets.js
@@ -4,9 +4,9 @@ const userHelper    = require("../lib/util/user-helper")
 
 const express       = require('express');
 const tweetsRoutes  = express.Router();
+const likesRoutes   = express.Router();
 
 module.exports = function(DataHelpers) {
-
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -17,19 +17,31 @@ module.exports = function(DataHelpers) {
     });
   });
 
+//
+
+  // tweetsRoutes.post("/likes", function(req, res) {
+  //   DataHelpers.likeTweet('5b082fdec21f9c3158ec80e0', (err) => {
+  //     if (err) {
+  //       res.status(500).json({ error: err.message });
+  //     } else {
+  //       res.status(201).send();
+  //     }
+  //   });
+  // });
+
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
     }
-
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
     const tweet = {
       user: user,
       content: {
-        text: req.body.text
+        text: req.body.text,
+        likes: 0
       },
-      created_at: Date.now()
+      created_at: new Date()
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
